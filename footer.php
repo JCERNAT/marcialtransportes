35cd8b0652738d62fd9412a468e9ee86c130c342
JCERNAT <149566271+JCERNAT@users.noreply.github.com>
2026-02-12 01:28:19 -0500

    Cambios

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="bg-dark text-white text-center p-4 mt-5">
-    © 2026 Marcial Transportes - Funkyboycod-dev Todos los derechos reservados
-</footer>
-
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
-</body>
-</html>
